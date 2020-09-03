23ed9f2370e6c10ed006e7102f23b28f2a37ee8b
Daniel Altman <Daniel.Altman4@gmail.com>
2020-09-03 01:01:56 -0700

    adding css and finsihing server.js

--- server.js
+++ server.js
@@ -1,2 +1,27 @@
 const express = require("express");
 
+const exphbs = require("express-handlebars");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+const routes = require("./controllers/burger_controller.js");
+app.use(routes);
+
+app.use(express.static("./public"));
+
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
